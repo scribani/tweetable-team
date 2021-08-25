6fb6aab1ba29803eaa353f7b89664db8a8c04c42
Scribani <scribani.andres@gmail.com>
2021-08-25 22:08:19 +0000

    Add css design system

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Route for Design system reference
+  get '/design', to: 'designs#index'
 end
