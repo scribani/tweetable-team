e2d2dc7b6fb7f4696a26d864846168ef72dcb5e5
Dulces123 <jregaladoantonio@gmail.com>
2021-08-25 20:58:39 +0000

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  #validations
+  # validations
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
   validates :name, presence: true
