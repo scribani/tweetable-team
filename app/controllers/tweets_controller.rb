55e22cfa3f701b8d087bc80037f0559333e500ed
Dulces123 <jregaladoantonio@gmail.com>
2021-08-26 03:52:02 +0000

    add new and edit tweets views

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -15,7 +15,7 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(body: params[:body], user_id: 1)
     if @tweet.save
-      redirect_to root_path
+      redirect_back fallback_location: '/'
     else
       render :new
     end
@@ -24,7 +24,7 @@ class TweetsController < ApplicationController
   def destroy
     @tweet = Tweet.find(params[:id])
     @tweet.destroy
-    redirect_to root_path
+    redirect_back fallback_location: '/'
   end
 
   # GET /tweets/:id/edit
