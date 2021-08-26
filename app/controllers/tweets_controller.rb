138b9cca450a9906e2a97050dd61428d67460bf9
alxdrian <alex_97piscis@outlook.com>
2021-08-26 01:23:33 +0000

    Add show to tweet controller and show details view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,9 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+    @replies = @tweet.replies
+  end
 end
