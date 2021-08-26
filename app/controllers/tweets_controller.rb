4aaaa2722d85ab33a6ec80d5a4039037c8e2a382
Dulces123 <jregaladoantonio@gmail.com>
2021-08-26 01:01:25 +0000

    add create and new methods to Tweet model

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,17 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(body: params[:body], user_id: current_user.id)
+    if @tweet.save
+      redirect_to root
+    else
+      render :new
+    end
+  end
 end
