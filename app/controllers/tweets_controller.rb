c21ab36cc52e8ce1f83342887ee081146eb63220
anaisabel <anaisbael.mts@gmail.com>
2021-08-26 01:51:38 +0000

    add edit and update methods for tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,12 +2,12 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
-  
+
   def show
     @tweet = Tweet.find(params[:id])
     @replies = @tweet.replies
   end
-  
+
   def new
     @tweet = Tweet.new
   end
@@ -19,5 +19,27 @@ class TweetsController < ApplicationController
     else
       render :new
     end
+  end
+
+  # GET /tweets/:id/edit
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  # PATCH/PUT /tweets/:id
+  def update
+    @tweet = Tweet.find(params[:id])
+
+    return redirect_to @tweet if @tweet.update(tweet_params)
+
+    render :edit
+  end
+
+  def destroy; end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body, :replied_to)
   end
 end
