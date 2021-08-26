b46336aca35b963cb01e95a673e3757dbe4a7c01
Dulces123 <jregaladoantonio@gmail.com>
2021-08-26 23:24:12 +0000

    permit name and username in signup user form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: %i[index show]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def index; end
+
+  def show; end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name])
+  end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,6 +19,7 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
     @tweet.user = current_user
+
     if @tweet.save
       redirect_back fallback_location: '/'
     else
