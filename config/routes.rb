3f1349b093ab1593a581622a6eb9037b333be441
alxdrian <alex_97piscis@outlook.com>
2021-08-26 00:10:29 +0000

    Add index to tweet controller and adds the tweet card in index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to 'tweets#index'
   resources :tweets
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Route for Design system reference
+  get '/design', to: 'designs#index'
 end
