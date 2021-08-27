3e46a55820d2ff99e7d62cdda384fb465d854ac1
alxdrian <alex_97piscis@outlook.com>
2021-08-27 03:22:53 +0000

    Add default checked in subnavabar tags

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
     return redirect_to root_path unless @user
+
     @tweets = Tweet.where(user_id: params[:id])
     @likes = Like.where(user_id: params[:id])
     @likeds = Tweet.where(id: @likes.pluck(:tweet_id))
